ea45262eeb97198a2fd3f9247430c2bc4093c3f7
henixmehta <henumehta03@gmail.com>
2024-10-10 00:59:59 +0530

    skillpage set

---
+++ Employee_management/src/main/java/repository/UserRepository.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/J2EE/EJB30/StatelessEjbClass.java to edit this template
+ */
+package repository;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Henil
+ */
+@Stateless
+public class UserRepository implements UserRepositoryLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
